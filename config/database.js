3e4734afa4dcba7d197c9def71ee43bbdf137284
nawan44 <rachmat.d.gunawan@gmail.com>
2021-02-06 00:00:11 +0700

    sequalize database

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+
+// Option 1: Passing parameters separately
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+});
+
+// Option 2: Passing a connection URI
+const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
